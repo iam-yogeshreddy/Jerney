cba6158ed6900ac290eb2dfce6bbedd7362213fe
Yogesh Reddy <136956748+iam-yogeshreddy@users.noreply.github.com>
2026-03-29 16:46:36 +0530

    Update VPC and EKS module sources to specific versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,8 +15,7 @@ locals {
 
 # ---- VPC ----
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=v5.0.0"
 
   name = "${var.cluster_name}-vpc"
   cidr = var.vpc_cidr
@@ -40,8 +39,7 @@ module "vpc" {
 
 # ---- EKS Cluster (Auto Mode) ----
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.31"
+  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-eks.git?ref=v20.31.0"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
